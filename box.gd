c1458f933a62fb0f3aa300adae52a04a72634b53
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-21 22:53:32 -0700

    js bug fixing

--- box.gd
+++ box.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
+@onready var player: CharacterBody2D = $"../player"
 
 var is_on_ground = true  
 func _physics_process(delta: float) -> void:
 	
-	if not is_on_floor():
+	if not is_on_floor() and !player.in_rotation :
 		is_on_ground = false
 		velocity += get_gravity() * delta
 	else: 
