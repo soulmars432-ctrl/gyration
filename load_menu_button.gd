84e3a08706523270946497ec405642c94f460fd1
soulmars432-ctrl <soulmars432@gmail.com>
2026-06-27 19:52:56 -0700

    lock menu added pictures

--- load_menu_button.gd
+++ load_menu_button.gd
@@ -1,16 +1,23 @@
 extends Button
 
 @export var scene:String
-@onready var click: AudioStreamPlayer = $"../click"
-@onready var cantclick: AudioStreamPlayer = $"../cantclick"
+@export var lock:ColorRect
+@onready var cantclick: AudioStreamPlayer = $"../../../cantclick"
+@onready var click: AudioStreamPlayer = $"../../../click"
 
 
-
+func _process(delta: float) -> void:
+	if scene in Lvlsfinished.lvls_unclocked:
+		lock.visible = false
+	else:
+		lock.visible = true
 
 func _on_pressed() -> void:
 	if scene in Lvlsfinished.lvls_unclocked:
+		lock.visible = false
 		click.play()
 		await click.finished
 		get_tree().change_scene_to_file(scene)
 	else:
+		lock.visible = true
 		cantclick.play()
